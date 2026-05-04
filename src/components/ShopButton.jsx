a1e30f47cb01a0e44eb0f8791f1bc0658e6d025e
editvagany <edit.vagany@innoveo.com>
2026-05-04 10:22:36 +0200

    add shoppingcart button

--- src/components/ShopButton.jsx
+++ src/components/ShopButton.jsx
@@ -1,20 +1,29 @@
-import MoneyImg from "../assets/Cupcake-coin.png"
+import MoneyImg from "../assets/Cupcake-coin.png";
+import CartImg from "../assets/Shoppingcart.png";
 
 export default function ShopButton({ money, onBuy }) {
-  return (
-    <div className="shopContainer">
+	return (
+		<div className="shopContainer">
+			<button
+				type="button"
+				onClick={onBuy}
+				className="shopButton"
+				disabled={money === 0}
+				aria-label="Spend money to buy muffins">
+				<span className="moneyCount">{money}</span>
+				<div className="moneyIcon">
+					<img src={MoneyImg} alt="coin" className="coinImg" />
+				</div>
+			</button>
       <button
         type="button"
-        onClick={onBuy}
-        className="shopButton"
-        disabled={money === 0}
-        aria-label="Spend money to buy muffins"
+        className="shopButton cartButton"
       >
-        <span className="moneyCount">{money}</span>
+        <span className="moneyCount">Shop</span>
         <div className="moneyIcon">
-            <img src={MoneyImg} alt="coin" className="coinImg"/>
+          <img src={CartImg} alt="cart" className="coinImg" />
         </div>
       </button>
-    </div>
-  );
+		</div>
+	);
 }
