c42e8bc3ad3231dabd316838d59ab5a1c4d7d2e0
editvagany <edit.vagany@innoveo.com>
2026-05-04 11:20:19 +0200

    add home routing to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,14 @@ import MuffinImg from "../assets/Vanilla-cupcake.png";
 export default function Header() {
 	return (
 		<header className="header">
-			<img className="navMuffin m1" src={MuffinImg} alt="" />
-			<img className="navMuffin m2" src={MuffinImg} alt="" />
-			<img className="navMuffin m3" src={MuffinImg} alt="" />
-			<img className="navMuffin m4" src={MuffinImg} alt="" />
 			<Link to="/" className="headerLink">
+				<img className="navMuffin m1" src={MuffinImg} alt="" />
+				<img className="navMuffin m2" src={MuffinImg} alt="" />
+				<img className="navMuffin m3" src={MuffinImg} alt="" />
+				<img className="navMuffin m4" src={MuffinImg} alt="" />
 				<h1>Welcome to the Muffin clicker</h1>
+				<p>Have fun!</p>
 			</Link>
-			<p>Have fun!</p>
 		</header>
 	);
 }
