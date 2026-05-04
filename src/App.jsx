2cd866d1ee2baebdea18ca3911a364bcf1692645
editvagany <edit.vagany@innoveo.com>
2026-05-04 13:26:46 +0200

    add shop items and adjust muffin placement

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,24 @@ import Muffin from "./components/Muffin";
 import ShopButton from "./components/ShopButton";
 import CounterDisplay from "./components/CounterDisplay";
 import Header from "./components/Header";
+import Shop from "./components/Shop";
 
-function Home({ counter, countClick }) {
+function Home({ counter, countClick, multiplier }) {
 	return (
 		<>
-			<CounterDisplay counter={counter} />
+			<CounterDisplay counter={counter} multiplier={multiplier}/>
 			<Muffin onClick={countClick} />
 		</>
 	);
 }
 
-function Shop() {
-	return <h2>Shop coming soon!</h2>;
-}
-
 function App() {
 	const [counter, setCounter] = useState(0);
 	const [money, setMoney] = useState(0);
+	const [multiplier, setMultiplier] = useState(1);
 
 	const countClick = () => {
-		const nextCounter = counter + 1;
+		const nextCounter = counter + multiplier;
 		setCounter(nextCounter);
 
 		if (nextCounter % 3 === 0) {
@@ -39,6 +37,13 @@ function App() {
 		setMoney(0);
 	};
 
+	const buyMultiplier = (value, cost) => {
+		if (money < cost) return;
+
+		setMoney((m) => m - cost);
+		setMultiplier(multiplier * value);
+	};
+
 	return (
 		<BrowserRouter>
 			<Header />
@@ -48,7 +53,10 @@ function App() {
 					path="/"
 					element={<Home counter={counter} countClick={countClick} />}
 				/>
-				<Route path="/shop" element={<Shop />} />
+				<Route
+					path="/shop"
+					element={<Shop money={money} buyMultiplier={buyMultiplier} />}
+				/>
 			</Routes>
 		</BrowserRouter>
 	);
--- src/components/CounterDisplay.jsx
+++ src/components/CounterDisplay.jsx
@@ -1,7 +1,12 @@
-export default function CounterDisplay({ counter }) {
-  return (
-    <div className="counterContainer">
-      <h2>{counter} muffin(s)</h2>
-    </div>
-  );
+export default function CounterDisplay({ counter, multiplier }) {
+	return (
+		<div className="container">
+			<div className="boosterContainer">
+				<h2>{multiplier}x booster active</h2>
+			</div>
+			<div className="counterContainer">
+				<h2>{counter} muffin(s)</h2>
+			</div>
+		</div>
+	);
 }
